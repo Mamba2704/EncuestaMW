9d1405b43e9c98564360c79403bd38ff18561abc
aborroni <andreaaborroni@gmail.com>
2024-01-10 10:03:26 -0300

    .
    .

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const checkbox = document.getElementById('checkotros');
-
+const checkboxmovil = document.getElementById('checkotrosmovil');
 const textarea = document.getElementById('textarea1');
 
+/*
+checkbox.addEventListener('click', function handleClick() {
+  if (checkbox.checked) {
+    textarea.style.visibility = 'visible';
+  } else {
+    textarea.style.visibility = 'hidden';
+  }
+});*/
 
 checkbox.addEventListener('click', function handleClick() {
   if (checkbox.checked) {
@@ -11,18 +19,19 @@ checkbox.addEventListener('click', function handleClick() {
   }
 });
 
-checkbox.addEventListener('touchstart', function handleClick() {
-  if (checkbox.checked) {
+checkboxmovil.addEventListener('touchstart', function handleClick() {
+  if (checkboxmovil.checked) {
     textarea.style.visibility = 'visible';
   } else {
     textarea.style.visibility = 'hidden';
   }
 });
 
+/*
  function otrosTextarea(index) {
   if (index === 4) {
     textarea.style.visibility = 'visible';
   } else {
     textarea.style.visibility = 'hidden';
   }
-};
+};*/
